4767b386aab9727afc886d514cda0bbe2b0447c9
임석현 (Andrew) <sjsh1623@gmail.com>
2024-08-09 14:02:40 +0900

    feat : Google API를 사용한 API 이슈  수정

--- src/api/google/place.js
+++ src/api/google/place.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
+const axios = require("axios");
 
 const ask = async (query) => {
-	const googleMapApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${process.env.GOOGLE_API_KEY}`;
-	const axios = require("axios");
+	const googleMapApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${process.env.GOOGLE_API_KEY}&query=${query}`;
 	try {
-		const api = await axios.post(googleMapApiUrl, {
-			query: query,
-		});
+		const api = await axios.post(googleMapApiUrl);
 		return api.data.results
 	} catch (error) {
 		console.error('Error occurred:', error.message);
