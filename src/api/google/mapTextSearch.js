b81fbeec343068fb724de3de1033dc8591f5779f
임석현 (Andrew) <sjsh1623@gmail.com>
2024-08-10 23:59:27 +0900

    feat : Data 전처리

--- src/api/google/mapTextSearch.js
+++ src/api/google/mapTextSearch.js
@@ -5,14 +5,14 @@ const ask = async (query) => {
 	const googleMapApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${process.env.GOOGLE_API_KEY}&query=${query}`;
 	try {
 		const response = await axios.post(googleMapApiUrl);
-		console.log(response.data)
-		return extractPlaceDetails(response.data);
+		if (response.data.status !== 'OK') return null;
+		return response.data.results.map(extractPlaceDetails);
 	} catch (error) {
 		console.error('Error occurred:', error.message);
 	}
 }
 
-const extractPlaceDetails = ({ formatted_address, name, rating, place_id, types ,user_ratings_total, geometry}) => ({
+const extractPlaceDetails = ({formatted_address, name, rating, place_id, types, user_ratings_total, geometry}) => ({
 	formatted_address,
 	name,
 	rating,
@@ -22,7 +22,7 @@ const extractPlaceDetails = ({ formatted_address, name, rating, place_id, types 
 	geometry,
 });
 
-module.exports = async (place, nation, city) => {
+module.exports = async (place, city) => {
 	const query = `${place} at ${city}`
 	return await ask(query)
 }
--- src/services/PlanService.js
+++ src/services/PlanService.js
@@ -5,7 +5,8 @@ const googleMapTextSearch = require('../api/google/mapTextSearch');
 const createPlan = async (location = {}) => {
 	const {nation, city} = location;
 	const url = 'BwUx4iAWiIo'
-	const places = await getPlacesFromYoutube(nation, city, url);
+	const places = await getPlacesFromYoutube('korea', 'seoul', url);
+	console.log(places)
 }
 
 const getPlacesFromYoutube = async (nation, city, url) => {
@@ -16,10 +17,11 @@ const getPlacesFromYoutube = async (nation, city, url) => {
 }
 
 const addInformationUsingGoogleMapApi = async (youTubeData, nation, city, url) => {
-	return await Promise.all(
+	const newDataArray = await Promise.all(
 		youTubeData.map(async (element) => {
-			const googlePlaceInfo = await googleMapTextSearch(element.place, nation, city);
-			return {
+			const googlePlaceInfos = await googleMapTextSearch(element.place, city);
+			if (!googlePlaceInfos || googlePlaceInfos.length === 0) return null;
+			return googlePlaceInfos.map((googlePlaceInfo) => ({
 				...element,
 				platform: 'youtube',
 				place: googlePlaceInfo.name,
@@ -32,12 +34,14 @@ const addInformationUsingGoogleMapApi = async (youTubeData, nation, city, url) =
 				place_id: googlePlaceInfo.place_id,
 				types: googlePlaceInfo.types,
 				coordinates: {
-					latitude: googlePlaceInfo.location.lat,
-					longitude: googlePlaceInfo.location.lng
+					latitude: googlePlaceInfo.geometry.location.lat,
+					longitude: googlePlaceInfo.geometry.location.lng
 				}
-			};
+			}));
 		})
 	);
+
+	return newDataArray.flat().filter(item => item !== null);
 };
 
 const savePlaces = (placesFromYoutube) => {
